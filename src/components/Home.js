df2abd142dffe307d79a8a7ea7c93f9f0dbd5f66
jonrutter <contact@jonrutter.com>
2021-10-07 19:57:59 -0400

    Add Searchbar, Grid, and Load More buttons to Homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,20 +11,49 @@ import { useHomeFetch } from '../hooks/useHomeFetch';
 
 // components
 import HeroImage from './HeroImage';
+import Grid from './Grid';
+import Thumb from './Thumb';
+import Spinner from './Spinner';
+import Searchbar from './Searchbar';
+import Button from './Button';
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
+  const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore } =
+    useHomeFetch();
   const { results: movies } = state;
-  console.log(movies);
+
+  if (error) return <div>Something went wrong ...</div>;
 
   return (
     <>
-      {movies[0] && (
+      {!searchTerm && movies[0] ? (
         <HeroImage
           image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${movies[0].backdrop_path}`}
           title={movies[0].title}
           text={movies[0].overview}
         />
+      ) : null}
+      <Searchbar setSearchTerm={setSearchTerm} />
+      <Grid header={searchTerm ? 'Search Results' : 'Popular Movies'}>
+        {movies.map((movie) => {
+          return (
+            <Thumb
+              key={movie.id}
+              clickable
+              image={
+                movie.poster_path
+                  ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                  : NoImage
+              }
+              movieId={movie.id}
+              title={movie.title}
+            />
+          );
+        })}
+      </Grid>
+      {loading && <Spinner />}
+      {state.page < state.total_pages && !loading && (
+        <Button text="Load More" callback={() => setIsLoadingMore(true)} />
       )}
     </>
   );
