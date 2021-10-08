6eb7b1132b453765995deab0d9f83f99e61b700c
jonrutter <contact@jonrutter.com>
2021-10-07 20:00:23 -0400

    Implement site routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 
+// routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 // components
 import Header from './components/Header';
 
 // Pages
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
-const App = () => {
-  return (
-    <div className="font-body">
+const App = () => (
+  <div className="font-body">
+    <Router>
       <Header />
-      <Home />
-    </div>
-  );
-};
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:movieId" element={<Movie />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  </div>
+);
 
 export default App;
