9968e673241c7557227a80b5f4613e38f6ba031c
jonrutter <contact@jonrutter.com>
2021-10-08 19:22:27 -0400

    Update site logo file

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 // logo
-import Logo from '../images/react-movie-logo.svg';
+import Logo from '../images/rfdb-logo.svg';
 import TMDBLogo from '../images/tmdb_logo.svg';
 
 const Header = () => (
   <div className="px-5 bg-gradient-to-br from-gray-900 to-gray-700">
-    <header className="max-w-screen-xl mx-auto py-4 flex justify-between">
+    <header className="max-w-screen-xl mx-auto py-5 flex justify-between">
       <Link to="/">
         <img src={Logo} alt="React Film DB Logo" className="h-auto site-logo" />
       </Link>
