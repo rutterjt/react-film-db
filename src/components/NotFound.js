988d400b6ed50b27b5091b682cdf9fdeee6148ce
jonrutter <contact@jonrutter.com>
2021-10-08 19:21:17 -0400

    Update 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NotFound = () => <div>Not Found</div>;
+const NotFound = () => (
+  <main>
+    404: Not found. Please return to the{' '}
+    <Link to="/" className="underline">
+      home page
+    </Link>
+    .
+  </main>
+);
 
 export default NotFound;
