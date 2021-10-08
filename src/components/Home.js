21db85200f00e2402dff3ac92f578b49ab2ab496
jonrutter <contact@jonrutter.com>
2021-10-08 19:22:10 -0400

    Add Footer, improve loading with spinner

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,45 +16,62 @@ import Thumb from './Thumb';
 import Spinner from './Spinner';
 import Searchbar from './Searchbar';
 import Button from './Button';
+import Footer from './Footer';
 
 const Home = () => {
-  const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore } =
-    useHomeFetch();
+  const {
+    state,
+    loading,
+    error,
+    searchTerm,
+    setSearchTerm,
+    setIsLoadingMore,
+    initialLoad,
+  } = useHomeFetch();
   const { results: movies } = state;
 
   if (error) return <div>Something went wrong ...</div>;
+  if (initialLoad)
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <Spinner />
+      </div>
+    );
 
   return (
     <>
-      {!searchTerm && movies[0] ? (
-        <HeroImage
-          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${movies[0].backdrop_path}`}
-          title={movies[0].title}
-          text={movies[0].overview}
-        />
-      ) : null}
-      <Searchbar setSearchTerm={setSearchTerm} />
-      <Grid header={searchTerm ? 'Search Results' : 'Popular Movies'}>
-        {movies.map((movie) => {
-          return (
-            <Thumb
-              key={movie.id}
-              clickable
-              image={
-                movie.poster_path
-                  ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
-                  : NoImage
-              }
-              movieId={movie.id}
-              title={movie.title}
-            />
-          );
-        })}
-      </Grid>
-      {loading && <Spinner />}
-      {state.page < state.total_pages && !loading && (
-        <Button text="Load More" callback={() => setIsLoadingMore(true)} />
-      )}
+      <main className="mb-16">
+        {!searchTerm && movies[0] ? (
+          <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${movies[0].backdrop_path}`}
+            title={movies[0].title}
+            text={movies[0].overview}
+          />
+        ) : null}
+        <Searchbar setSearchTerm={setSearchTerm} />
+        <Grid header={searchTerm ? 'Search Results' : 'Popular Movies'}>
+          {movies.map((movie) => {
+            return (
+              <Thumb
+                key={movie.id}
+                clickable
+                image={
+                  movie.poster_path
+                    ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                    : NoImage
+                }
+                movieId={movie.id}
+                title={movie.title}
+              />
+            );
+          })}
+        </Grid>
+        {loading && <Spinner />}
+        {state.page < state.total_pages && !loading && (
+          <Button text="Load More" callback={() => setIsLoadingMore(true)} />
+        )}
+      </main>
+      <Footer />
     </>
   );
 };
