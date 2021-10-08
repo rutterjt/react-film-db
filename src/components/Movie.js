5ea34e0aa0e8127ece20046fe4c68f2ab334fbcb
jonrutter <contact@jonrutter.com>
2021-10-08 19:21:58 -0400

    Add Footer, improve loading with spinner

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // routing
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 // config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
@@ -13,6 +13,7 @@ import BreadCrumb from './BreadCrumb';
 import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
 import Actor from './Actor';
+import Footer from './Footer';
 
 // hook
 import { useMovieFetch } from '../hooks/useMovieFetch';
@@ -25,32 +26,51 @@ const Movie = () => {
   const { movieId } = useParams();
   const { state: movie, loading, error } = useMovieFetch(movieId);
 
-  if (loading) return <Spinner />;
-  if (error) return <div>Something went wrong...</div>;
+  console.log(movie);
+
+  if (loading)
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <Spinner />
+      </div>
+    );
+  if (error)
+    return (
+      <div>
+        Something went wrong. Please try going back to the{' '}
+        <Link to="/" className="underline">
+          home page
+        </Link>
+        .
+      </div>
+    );
 
   return (
     <>
-      <BreadCrumb movieTitle={movie.title} />
-      <MovieInfo movie={movie} />
-      <MovieInfoBar
-        time={movie.runtime}
-        budget={movie.budget}
-        revenue={movie.revenue}
-      />
-      <Grid header="Cast">
-        {movie.actors.map((actor) => (
-          <Actor
-            key={actor.credit_id}
-            name={actor.name}
-            character={actor.character}
-            imageUrl={
-              actor.profile_path
-                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
-                : NoProfileImage
-            }
-          />
-        ))}
-      </Grid>
+      <main>
+        <BreadCrumb movieTitle={movie.title} />
+        <MovieInfo movie={movie} />
+        <MovieInfoBar
+          time={movie.runtime}
+          budget={movie.budget}
+          revenue={movie.revenue}
+        />
+        <Grid header="Cast">
+          {movie.actors.map((actor) => (
+            <Actor
+              key={actor.credit_id}
+              name={actor.name}
+              character={actor.character}
+              imageUrl={
+                actor.profile_path
+                  ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                  : NoProfileImage
+              }
+            />
+          ))}
+        </Grid>
+      </main>
+      <Footer />
     </>
   );
 };
