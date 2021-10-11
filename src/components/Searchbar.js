9fb28e570aa955e94e74fee231b406a00327e583
jonrutter <contact@jonrutter.com>
2021-10-11 15:40:03 -0400

    Convert inline handler to curried function

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -18,14 +18,16 @@ const Searchbar = ({ setSearchTerm }) => {
     return () => clearTimeout(timeout);
   }, [setSearchTerm, state]);
 
+  const handle = (setter) => (e) => setter(e.currentTarget.value);
+
   return (
     <div className="bg-gradient-to-br from-gray-900 to-gray-700 py-5 flex item-center h-24 px-5">
       <div className="text-white relative max-w-screen-xl w-full h-full mx-auto bg-gray-700 rounded-full">
         <input
           className="block h-full bg-transparent text-xl absolute left-0 px-16 w-full outline-none rounded-full focus:ring-2 focus:ring-gray-400"
           type="text"
-          placeholder="Search"
-          onChange={(e) => setState(e.currentTarget.value)}
+          placeholder="Search for movies"
+          onChange={handle(setState)}
           value={state}
         />
         <FaSearch className="absolute left-4 top-4 text-2xl" />
