5eb7df66d684262b05f506e08034e16db7a6515d
jonrutter <contact@jonrutter.com>
2021-10-11 13:51:58 -0400

    Prevent TMDB logo from disappearing on mobile

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import { ReactComponent as TMDBLogo } from '../images/tmdb_logo-full.svg';
+import TMDBLogo from '../images/tmdb_logo-full.svg';
 
 const Attribution = () => {
   return (
     <div className="flex max-w-xs items-center">
-      <TMDBLogo className="h-auto tmdb-logo mr-4" />
+      <img
+        src={TMDBLogo}
+        alt="The Movie Database logo"
+        className="tmdb-logo mr-4"
+      />
       <p>
         This website uses the TMDB API but is not endorsed or certified by TMDB.
       </p>
