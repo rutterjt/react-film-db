288f3c70473af7c626e21fb803cb9bfbe3c88bcf
Jon Rutter <contact@jonrutter.com>
2022-01-21 21:14:11 -0500

    Create base theme files

---
+++ src/styles/palette.js
@@ -0,0 +1,8 @@
+export const palette = {
+  gray: { 300: '#D1D5DB', 700: '#374151', 800: '#1F2937', 900: '#0F172A' },
+};
+
+export const gradients = {
+  searchBar: `linear-gradient(to bottom right, ${palette.gray['700']}, ${palette.gray['900']});`,
+  header: `linear-gradient(to bottom right, ${palette.gray['900']}, ${palette.gray['700']});`,
+};
