cbdcf03e7051e3b1b46bcd9a3be75590a28dd849
Jon Rutter <contact@jonrutter.com>
2022-03-25 14:45:09 -0400

    Add site footer to app wrapper

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // components
 import ScrollToTop from './components/ScrollToTop';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 // Pages
 import Home from './pages/Home';
@@ -21,6 +22,7 @@ const App = () => (
       <Route path="/:movieId" element={<Movie />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
+    <Footer />
   </Router>
 );
 
