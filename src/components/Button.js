edf7a19785027d323e777fd6850fd79fc25dc5a6
Jon Rutter <contact@jonrutter.com>
2022-03-25 14:44:03 -0400

    Add custom focus styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = ({ text, onClick }) => (
   <button
     onClick={onClick}
-    className="inline-block bg-slate-700 text-slate-50 py-3 px-8 rounded-2xl hover:opacity-80 transition-opacity text-lg"
+    className="inline-block bg-slate-700 text-slate-50 py-3 px-8 rounded-2xl hover:opacity-80 focus:opacity-80 transition-opacity text-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-white"
   >
     {text}
   </button>
