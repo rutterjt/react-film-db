b18c396985753e10356b8a6f8a74b6c11c401248
Jon Rutter <contact@jonrutter.com>
2022-03-25 14:46:36 -0400

    Uninstall styled-components

--- src/index.js
+++ src/index.js
@@ -5,17 +5,11 @@ import '@fontsource/open-sans';
 
 import App from './App';
 
-import CSSReset from './styles/globalStyles';
-import { GlobalThemeProvider } from './styles/theme';
-
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalThemeProvider>
-      <CSSReset />
-      <App />
-    </GlobalThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
